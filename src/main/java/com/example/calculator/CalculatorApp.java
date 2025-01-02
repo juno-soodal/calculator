629f5fcd159abd0d127a92c9a4d63bbe1a0d690c
jun5-soodal <j01024749503@gmail.com>
2025-01-02 13:12:51 +0900

    refactor: 중복 부분 메서드로 추출

--- src/main/java/com/example/calculator/CalculatorApp.java
+++ src/main/java/com/example/calculator/CalculatorApp.java
@@ -4,60 +4,17 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class CalculatorApp {
+    private static final Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         while (true) {
-            Integer firstNumber =null ;
-            while (true) {
-                try {
-                    System.out.print("첫번째 숫자: ");
-                    firstNumber = scanner.nextInt();
-                    break;
-                } catch (InputMismatchException e) {
-                    System.out.println("숫자만 입력 가능합니다.");
+            Integer firstNumber = getNumber("첫번째 숫자: ");
+            Integer secondNumber = getNumber("두번째 숫자: ");
+            String operator = getOperator();
+            double result = calculate(operator, firstNumber, secondNumber);
 
-                }finally {
-                    scanner.nextLine();
-                }
-            }
-            Integer secondNumber = null;
-            while (true) {
-                try {
-                    System.out.print("두번째 숫자: ");
-                    secondNumber = scanner.nextInt();
-                    break;
-                } catch (InputMismatchException e) {
-                    System.out.println("숫자만 입력 가능합니다.");
-                }finally {
-                    scanner.nextLine();
-                }
-            }
-            String operator = null;
-            while (true) {
-                System.out.print("연산기호: ");
-                operator = scanner.nextLine();
-                if (operator.equals("+") || operator.equals("-") || operator.equals("*") || operator.equals("/")) {
-                    break;
-                }
-                System.out.println("연산기호는 + , - , * , / 중에서 입력하세요");
-            }
-
-            double result = 0;
-            switch (operator) {
-                case "+" :
-                    result =firstNumber + secondNumber;
-                    break;
-                case "-":
-                    result = firstNumber - secondNumber;
-                    break;
-                case "*":
-                    result = firstNumber * secondNumber;
-                    break;
-                case "/":
-                    result = (double) firstNumber / secondNumber;
-                    break;
-            }
             System.out.println(firstNumber + " " + operator + " " + secondNumber + " = "  + result);
+
             System.out.print("더 계산하시겠습니까? (exit 입력 시 종료)");
             String exit = scanner.nextLine();
             if (exit.equals("exit")) {
@@ -65,5 +22,51 @@ public class CalculatorApp {
             }
         }
 
+    }
+
+    private static double calculate(String operator, Integer firstNumber, Integer secondNumber) {
+        switch (operator) {
+            case "+" :
+                return firstNumber + secondNumber;
+            case "-":
+                return  firstNumber - secondNumber;
+            case "*":
+                return  firstNumber * secondNumber;
+            case "/":
+                return  (double) firstNumber / secondNumber;
+            default:
+                return 0;
+        }
+    }
+
+
+    private static Integer getNumber(String message) {
+        Integer firstNumber = null;
+        while (true) {
+            try {
+                System.out.print(message);
+                firstNumber = scanner.nextInt();
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("숫자만 입력 가능합니다.");
+
+            } finally {
+                scanner.nextLine();
+            }
+        }
+        return firstNumber;
+    }
+
+    private static String getOperator() {
+        String operator;
+        while (true) {
+            System.out.print("연산기호: ");
+            operator = scanner.nextLine();
+            if (operator.equals("+") || operator.equals("-") || operator.equals("*") || operator.equals("/")) {
+                break;
+            }
+            System.out.println("연산기호는 + , - , * , / 중에서 입력하세요");
+        }
+        return operator;
     }
 }
